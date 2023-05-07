8290c33793fb4542b75c04036cdbcebfdc69df31
AlwaysSayNo <allbitxxx@gmail.com>
2023-05-07 14:24:51 +0300

    - Removed initial information from app files. - Created new component course-template

---
+++ src/app/course-template/course-template.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-course-template',
+  templateUrl: './course-template.component.html',
+  styleUrls: ['./course-template.component.scss']
+})
+export class CourseTemplateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
